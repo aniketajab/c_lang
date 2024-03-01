c3cf924750e024f91263fe27528b35a0d201cbd8
aniketajab <161799391+aniketajab@users.noreply.github.com>
2024-03-01 10:38:43 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+    int main()
+    {
+int arr[5];
+arr[0]=10;
+arr[1]=20;
+arr[2]=30;
+arr[3]=40;
+arr[4]=50;
+printf("element 1: %d\n",arr[0]);
+printf("element 2: %d\n",arr[1]);
+printf("element 3: %d\n",arr[2]);
+printf("element 4: %d\n",arr[3]);
+printf("element 5: %d\n",arr[4]);
+    }
